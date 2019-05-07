7113c5b7680813bf2f4ea08a7c6170a8003deb87
Oleksandr Herashchenko <s238155@studenti.polito.it>
2019-05-07 16:22:24 +0200

    lezione 07/05

--- src/it/polito/tdp/metroparis/model/Model.java
+++ src/it/polito/tdp/metroparis/model/Model.java
@@ -1,59 +1,143 @@
 package it.polito.tdp.metroparis.model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 import org.jgrapht.Graph;
 import org.jgrapht.Graphs;
+import org.jgrapht.event.ConnectedComponentTraversalEvent;
+import org.jgrapht.event.EdgeTraversalEvent;
+import org.jgrapht.event.TraversalListener;
+import org.jgrapht.event.VertexTraversalEvent;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleDirectedGraph;
+import org.jgrapht.traverse.DepthFirstIterator;
+import org.jgrapht.traverse.GraphIterator;
 
 import it.polito.tdp.metroparis.db.MetroDAO;
 
 public class Model {
 	
+	private class EdgeTraversedGraphListener implements TraversalListener<Fermata, DefaultEdge> {
+		@Override
+		public void connectedComponentFinished(ConnectedComponentTraversalEvent arg0) {	
+		}
+
+		@Override
+		public void connectedComponentStarted(ConnectedComponentTraversalEvent arg0) {
+		}
+
+		@Override
+		public void edgeTraversed(EdgeTraversalEvent<DefaultEdge> e) {
+			
+	/*		back codifica relazioni del tipo child --> parent
+	 * 
+	 *      per un nuovo vertice child scoperto devo avere che:
+	 *      - child è ancora sconosciuto (non ancora trovato)
+	 *      - parent è già stato visitato
+	*/		
+			Fermata sourceVertex = grafo.getEdgeSource(e.getEdge());
+			Fermata targetVertex = grafo.getEdgeTarget(e.getEdge());
+			
+	/*      se il grafo è orientao, allora source==parent, target==child
+	 *      se il grafo non è orientato, potrebbe essere al contrario...
+	 */
+			if (!backVisit.containsKey(targetVertex) && backVisit.containsKey(sourceVertex))
+				backVisit.put(targetVertex, sourceVertex);
+			else if (!backVisit.containsKey(sourceVertex) && backVisit.containsKey(targetVertex))
+				backVisit.put(sourceVertex, targetVertex);
+			
+		}
+
+		@Override
+		public void vertexFinished(VertexTraversalEvent<Fermata> arg0) {	
+		}
+
+		@Override
+		public void vertexTraversed(VertexTraversalEvent<Fermata> arg0) {	
+		}
+		
+	}
+	
 	private Graph<Fermata, DefaultEdge> grafo;
 	private List<Fermata> fermate;
 	private Map<Integer, Fermata> fermateIdMap;
+	private Map<Fermata, Fermata> backVisit;
 	
 	public void creaGrafo() {
 		
-		// creo il grafo
+//      creo il grafo
 		grafo = new SimpleDirectedGraph<>(DefaultEdge.class);
 		
-		// aggiungo i vertici
+//      aggiungo i vertici
 		MetroDAO dao = new MetroDAO();
 		fermate = dao.getAllFermate();
 		Graphs.addAllVertices(grafo, fermate);
 		
-		// creo idMap
+//      creo idMap
 		fermateIdMap = new HashMap<>() ;
 		for (Fermata f : fermate)
 			fermateIdMap.put(f.getIdFermata(), f) ;
 		
-		// aggiungo gli archi:
+//      aggiungo gli archi:
 		
-		/* 1° opzione: per ogni coppia di vertici fare una query per determinare se esiste l'arco o meno
+/*      1° opzione: per ogni coppia di vertici fare una query per determinare se esiste l'arco o meno
 		for (Fermata partenza : grafo.vertexSet())
 			for (Fermata arrivo : grafo.vertexSet())
 				if (dao.esisteConnessione(partenza, arrivo))
 					grafo.addEdge(partenza, arrivo);
-		*/
+*/
 		
-		/* 2° opzione: per ogni vertice fare una query che restituisca l'elenco di tutti i vertici che dovranno essere adiacenti
+//		2° opzione: per ogni vertice fare una query che restituisca l'elenco di tutti i vertici che dovranno essere adiacenti
 		for (Fermata partenza : grafo.vertexSet()) {
 			List<Fermata> arrivi = dao.stazioniArrivo(partenza, fermateIdMap);
 			
 			for (Fermata arrivo : arrivi)
 				grafo.addEdge(partenza, arrivo);
 		}
-		*/
 		
-		// 3° opzione: singola query che produce "subito" tutto il grafo
+//      3° opzione: singola query che produce "subito" tutto il grafo
 		
+	}
+	
+	public List<Fermata> fermateRaggiungibili(Fermata source) {
+		List<Fermata> result = new ArrayList<Fermata>();
+		backVisit = new HashMap<>();
 		
+//		GraphIterator<Fermata, DefaultEdge> it = new BreadthFirstIterator<>(grafo, source);
+		GraphIterator<Fermata, DefaultEdge> it = new DepthFirstIterator<>(grafo, source);
 		
+		it.addTraversalListener(new Model.EdgeTraversedGraphListener());
+		
+		backVisit.put(source, null);  // inseriamo la radice: non ha nessun padre
+		
+		while (it.hasNext()) {
+			result.add(it.next());
+		}
+		
+//		System.out.println(backVisit);
+		
+		return result;
+	}
+	
+	public List<Fermata> percorsoFinoA(Fermata target) {
+		if (!backVisit.containsKey(target)) {
+//			il target non è raggiungibile dalla source
+			return null;
+		}
+		
+		List<Fermata> percorso = new LinkedList<>();
+		Fermata f = target;
+		
+		while (f != null) {
+			percorso.add(0, f);
+			f = backVisit.get(f);
+		}
+		
+		return percorso;
 	}
 	
 	public Graph<Fermata, DefaultEdge> getGrafo() {
--- src/it/polito/tdp/metroparis/model/TestModel.java
+++ src/it/polito/tdp/metroparis/model/TestModel.java
@@ -1,5 +1,7 @@
 package it.polito.tdp.metroparis.model;
 
+import java.util.List;
+
 public class TestModel {
 	
 	public static void main(String[] args) {
@@ -8,7 +10,19 @@ public class TestModel {
 		
 		m.creaGrafo();
 		
-		System.out.println(m.getGrafo());
+//		System.out.println(m.getGrafo());
+		
+		System.out.format("Creati %d vertici e %d archi\n", m.getGrafo().vertexSet().size(), m.getGrafo().edgeSet().size());
+		
+		Fermata source = m.getFermate().get(0);
+		System.out.println("\nParto da: "+source);
+		List<Fermata> raggiungibili = m.fermateRaggiungibili(source);
+		System.out.println("Fermate raggiunte: "+raggiungibili+" ("+raggiungibili.size()+")");
+		
+		Fermata target = m.getFermate().get(150);
+		System.out.println("\nArrivo a: "+target);
+		List<Fermata> percorso = m.percorsoFinoA(target);
+		System.out.println("Percorso: "+percorso);
 		
 	}
 
